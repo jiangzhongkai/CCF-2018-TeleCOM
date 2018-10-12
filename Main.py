cc45e261bdb0872adefd8767f22e5f1cf67e6326
Peter_Bonnie <865268033@qq.com>
2018-10-12 21:47:01 +0800

    main func

---
+++ Main.py
@@ -0,0 +1,119 @@
+from data_processing import *
+from xgb_model import *
+from TimeCost import *
+from sklearn.model_selection import StratifiedKFold
+from RemoveFeatures import *
+
+if __name__ == '__main__':
+
+
+    mode = True
+    mode = False
+    tc = TimeCost()
+    dp = data_processing(mode)
+    df, dft = dp.data_input('../data/train_pro_3.csv', '../data/test_pro_3.csv')
+    tc.print_event()
+
+    df=df.replace("\\N",-1)
+    dft=dft.replace("\\N",-1)
+    #对性别和年龄进行处理
+    if 'age' in df.columns:
+        #做相应的处理
+        df['age']=df['age'].replace("\\N",-1)
+        df['age']=df['age'].astype(np.int)
+        dft['age']=dft['age'].astype(np.int)
+
+    if  '2_total_fee' in df.columns and '3_total_fee' in df.columns:
+        df['2_total_fee']=df['2_total_fee'].astype(np.float64)
+        df['3_total_fee']=df['3_total_fee'].astype(np.float64)
+
+        dft['2_total_fee'] = dft['2_total_fee'].astype(np.float64)
+        dft['3_total_fee'] = dft['3_total_fee'].astype(np.float64)
+
+    if 'gender' in df.columns:
+        #做相应的处理
+        df['gender'] = df['gender'].replace("1", 1).astype(int)
+        df['gender'] = df['gender'].replace("0", 0).astype(int)
+        df['gender'] = df['gender'].replace("00", 0).astype(int)
+        df['gender'] = df['gender'].replace("01", 1).astype(int)
+        df['gender'] = df['gender'].replace("02", 2).astype(int)
+        df['gender'] = df['gender'].replace("2", 2).astype(int)
+        df['gender'] = df['gender'].replace("\\N", 0).astype(int)
+
+    print("df.columns:",df.columns)
+    print("dft.columns:",dft.columns)
+    #将不是类别变量的特征进行标准化处理
+    # df, dft = fe.feature_process(df, dft)
+    rm_feats=RemoveFeat()
+    df=rm_feats.remove_feats(df,['former_complaint_num',
+                      # 'pay_times',   #本次添加
+                      'complaint_level',
+                      'former_complaint_fee',
+                      'many_over_bill',
+                      # 'is_promise_low_consume',  #本次添加
+                      #'service_type',  # 添加了
+                      'is_mix_service',
+                      #"gender",    #去掉gender试试
+                      'net_service'])
+    dft=rm_feats.remove_feats(dft,['former_complaint_num',
+                      # 'pay_times',   #本次添加
+                      'complaint_level',
+                      'former_complaint_fee',
+                       'many_over_bill',
+                      # 'is_promise_low_consume',  #本次添加
+                      #'service_type',  # 添加了
+                      'is_mix_service',
+                      #"gender",    #去掉gender试试
+                      'net_service'])
+    #给类别变量添加标签，转化为连续的值
+    print("dft:",dft.columns)
+    print("df:",df.columns)
+    tc.print_event()
+
+    #这里用了两种方法进行交叉验证，使用分层抽样，则flag=True,否则组验证，则flag=False
+    flag=False
+    if flag:
+        #进行参数配置
+        n_splits=5
+        random_state=42
+        shuffle=True
+        skr=StratifiedKFold(n_splits=n_splits,random_state=random_state,shuffle=shuffle)
+        y=df[['current_service']]
+        X=df.drop(['current_service'],axis=1)
+        # X_train,y_train,X_valid,y_valid,X_test=dp.get_split_data_1(df,dft,skr)  #获得交叉验证后的数据
+        for index,(train_index,valid_index) in enumerate(skr.split(X,y)):
+            x_train = X.iloc[train_index]
+            x_valid = X.iloc[valid_index]
+            y_train=y.iloc[train_index]
+            y_valid=y.iloc[valid_index]
+            x_test=dft
+            xgb=xgb_model(mode)
+            result=xgb.train_model(x_train,y_train,x_valid,y_valid,x_test)
+            if index == 0:
+                cv_pred = np.array(result).reshape(-1, 1)
+            else:
+                cv_pred = np.hstack((cv_pred, np.array(result).reshape(-1, 1)))
+    else:
+        X_train, y_train, X_valid, y_valid, X_test = dp.get_split_data(df, dft)
+        tc.print_event()
+
+        xgb = xgb_model(mode)
+        result = xgb.train_model(X_train, y_train, X_valid, y_valid, X_test)
+        tc.print_event()
+
+        if not mode:
+            dp.transform_index(result)
+    # xgb.load_model()
+    #投票
+    # if len(cv_pred)!=0:
+    #     submit = []
+    #     for line in cv_pred:
+    #         submit.append(np.argmax(np.bincount(line)))  # 统计出现的次数
+
+        #提交结果
+        # dp.transform_index(submit)
+
+    #最后的结束时间
+    # tc.print_event()
+
+
